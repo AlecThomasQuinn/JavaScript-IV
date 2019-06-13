776d637d14bcf194b3a1b09db2dbfb9e7f10bb24
unknown <alec.thomas.quinn@gmail.com>
2019-06-13 16:06:29 -0500

    Classes and functions made (trouble w/listsSubjects function), beggining tests

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,89 @@
 // CODE here for your Lambda Classes
+
+//Person, Instructor, Student, PM
+
+// Person
+// name, age, location
+// speak()
+
+class Person{
+    constructor(propsPerson){
+        this.name = propsPerson.name
+        this.age = propsPerson.age
+        this.location = propsPerson.location
+    }
+
+    speak(){
+        return `Hello, my name is ${this.name} and I am from ${this.location}.`
+    }
+}
+
+// Instructor
+// specialty, favLanguage, catchPhrase
+// demo(subject)
+// grade(student, subject)
+
+class Instructor extends Person{
+    constructor(propsInstructor){
+        super(propsPerson)
+        this.specialty = propsInstructor.specialty
+        this.favLanguage = propsInstructor.favLanguage
+        this.catchPhrase = propsInstructor.catchPhrase
+    }
+
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+// Student
+// previousBackground, className, favSubjects
+// listsSubjects()
+// PRAssignment()
+// sprintChallenge()
+
+class Student extends Person{
+    constructor(propsPerson){
+        super(propsStudent)
+        this.previousBackground = propsStudent.previousBackground
+        this.className = propsStudent.className
+        this.favSubjects = propsStudent.favSubjects
+    }
+
+    //listsSubjects(){}
+    
+    PRAssignments(subject){
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+
+    sprintChallenge(subject){
+        return `${this.name} has begun the Sprint Challenge for ${subject}`
+    }
+}
+
+
+// Project Manager
+// gradClassName, favInstructor
+// standUp()
+// debugsCode()
+
+class PM extends Instructor{
+    constructor(propsPM){
+        super(propsInstructor)
+        this.gradClassName = propsPM.gradClassName
+        this.favInstructor = propsPM.favInstructor
+    }
+
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standup!​​​​​`
+    }
+
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
